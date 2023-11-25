1bf7863f290be6626bdc2a626b853f3fc621f648
Viana Trinh <vuaivan.trinh@sjsu.edu>
2023-11-25 01:27:46 -0800

    Modify AndExpr , Or Expr, StringEXpr

--- MyExprVisitor.py
+++ MyExprVisitor.py
@@ -1,3 +1,4 @@
+from ExprLexer import ExprLexer
 from ExprParser import ExprParser
 from ExprVisitor import ExprVisitor
 
@@ -6,20 +7,64 @@ class MyExprVisitor(ExprVisitor):
     def __init__(self):
         super(MyExprVisitor, self).__init__()
         self.stack = []  # Stack to evaluate the expression
+        self.variables = {}  # Dictionary to store variables
+        self.variables_list = []
+        self.type_value_dict = {}
+        # for DEF parametres
+        self.buffer_pare = {}
+        # for -- / ++ operators
+        self.operator_pare = {}
+        # dor IF / WHILE operators
+        self.operator = False
 
     # Visit a parse tree produced by ExprParser#prog.
     def visitProg(self, ctx: ExprParser.ProgContext):
-        return self.visit(ctx.expr())  # Just visit the self expression
+        if ctx.expr():  # Check if there are any expressions
+            return self.visit(ctx.expr(0))  # Visit the first expression
 
+    # Visit a parse tree produced by ExprParser#numberExpr.
+    def visitNumberExpr(self, ctx):
+        if ctx.INT():
+            c = int(str(ctx.INT()))
+        elif ctx.FLOAT():
+            c = float(str(ctx.FLOAT()))
+
+        self.stack.append(c)
+
+        return c
+
+    # Visit a parse tree produced by ExprParser#parensExpr.
+    def visitParenExpr(self, ctx):
+        return self.visit(ctx.expr())  # Since enclosed by parents, just visit expr
+
+    # Visit a parse tree produced by ExprParser#
+
+    # Override visitStringExpr
+    def visitStringExpr(self, ctx):
+        self.stack.append(str(ctx.STRING().getText()))
+        return str(ctx.STRING().getText())
+
+    # Override visitVariableExpr
+    def visitVariableExpr(self, ctx):
+        variable_name = ctx.ID().getText()
+        # You may want to handle variable lookup and push its value onto the stack here
+
+        self.stack.append(variable_name)
+
+    # Override visitInfixExpr for binary operations
     # Visit a parse tree produced by ExprParser#infixExpr.
-    def visitInfixExpr(self, ctx: ExprParser.InfixExprContext):
+    def visitInfixExpr(self, ctx):
         self.visit(ctx.left)  # Evaluate the left  expression and push to stack
         self.visit(ctx.right)  # Evaluate the right expression and push to stack
+
         b = self.stack.pop()  # Why is ‘b’ the first popped item?
         a = self.stack.pop()
         c = None
 
-        if ctx.OP_ADD():
+        if ctx.OP_POW():
+            c = a ** b
+
+        elif ctx.OP_ADD():
             c = a + b
         elif ctx.OP_SUB():
             c = a - b
@@ -27,18 +72,47 @@ class MyExprVisitor(ExprVisitor):
             c = a * b
         elif ctx.OP_DIV():
             c = a / b
-        elif ctx.OP_POW():
-            c = a ** b
+        elif ctx.OP_MOD():
+            c = a % b
+        elif ctx.OP_FLOOR_DIV():
+            c = a // b
 
         self.stack.append(c)
         return c
 
-    # Visit a parse tree produced by ExprParser#numberExpr.
-    def visitNumberExpr(self, ctx: ExprParser.NumberExprContext):
-        c = int(str(ctx.INT()))  # Found a number, just insert to stack
+    def visitAndExpr(self, ctx: ExprParser.AndExprContext):
+        self.visit(ctx.left)
+        self.visit(ctx.right)
+        b = self.stack.pop()
+        a = self.stack.pop()
+        c = a and b
+
+        if a != b:
+            c = False
+
         self.stack.append(c)
         return c
 
-    # Visit a parse tree produced by ExprParser#parensExpr.
-    def visitParensExpr(self, ctx: ExprParser.ParensExprContext):
-        return self.visit(ctx.expr())  # Since enclosed by parents, just visit expr
+    def visitOrExpr(self, ctx: ExprParser.OrExprContext):
+        self.visit(ctx.left)
+        self.visit(ctx.right)
+        b = self.stack.pop()
+        a = self.stack.pop()
+        c = a or b
+        if a != b:
+            c = True
+
+        self.stack.append(c)
+        return c
+
+    def visitCommentExpr(self, ctx: ExprParser.CommentExprContext):
+        pass
+
+    def visitStringExpr(self, ctx):
+        string_literal = str(ctx.STRING().getText())  # Get the raw string with quotes
+        # Remove the quotes at the beginning and end of the string
+        string_value = string_literal[1:-1]
+        self.stack.append(string_value)
+        return string_value
+
+
